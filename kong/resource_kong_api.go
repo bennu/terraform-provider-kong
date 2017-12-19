84ffe01f8cdacb889886d574e815df201a2753a3
bugbuilder <bennu@bennu.cl>
2017-12-19 17:59:44 -0300

    adding resource kong api create

---
+++ kong/resource_kong_api.go
@@ -0,0 +1,79 @@
+package kong
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/bennu/kong-go/client"
+	"github.com/bennu/kong-go/types"
+	"github.com/hashicorp/terraform/helper/schema"
+)
+
+func resourceKongAPI() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceKongAPICreate,
+		Read:   resourceKongAPIRead,
+		Delete: resourceKongAPIDelete,
+		Update: resourceKongAPIUpdate,
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+
+			"uris": {
+				Type:     schema.TypeList,
+				Required: true,
+				ForceNew: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			"upstream_url": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+		},
+	}
+}
+
+func resourceKongAPICreate(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*client.Client)
+	req := types.APICreate{}
+
+	if v, ok := d.GetOk("uris"); ok {
+		var uris []string
+		for _, tag := range v.([]interface{}) {
+			uris = append(uris, tag.(string))
+		}
+		req.URIS = uris
+	}
+	req.UpstreamURL = d.Get("upstream_url").(string)
+
+	api, err := client.APICreate(context.Background(), d.Get("name").(string), req)
+	if err != nil {
+		return fmt.Errorf("Error creating API: %s", err)
+	}
+
+	d.SetId(api.Id)
+
+	return resourceKongAPIRead(d, meta)
+}
+
+func resourceKongAPIRead(d *schema.ResourceData, meta interface{}) error {
+	client := meta.(*client.Client)
+
+	_, err := client.APILookup(context.Background(), d.Id())
+	if err != nil {
+		return fmt.Errorf("Error retrieving API: %s", err)
+	}
+
+	return nil
+}
+
+func resourceKongAPIDelete(d *schema.ResourceData, meta interface{}) error {
+	return nil
+}
+func resourceKongAPIUpdate(d *schema.ResourceData, meta interface{}) error {
+	return nil
+}
